fcf52d984c2caf7aed9e402e89c5ac034a14ac08
gabriel <gabriel.m.ledesma96@gmail.com>
2023-11-21 18:00:06 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/taskform/TaksForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TaksForm = () => {
+  return (
+    <div>TaksForm</div>
+  )
+}
+
+export default TaksForm
